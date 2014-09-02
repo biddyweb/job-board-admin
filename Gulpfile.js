a6a2568bf6bf4c9a6304d7cfdd2c5577b29c7872
Gabriel Chertok <gchertok@gmail.com>
2014-09-02 18:16:00 -0300

    added build / deploy scripts

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,7 @@
 var gulp      = require('gulp'),
   gutil       = require('gulp-util'),
-  styl        = require('gulp-styl'),
-  csso        = require('gulp-csso'),
+  /*styl        = require('gulp-styl'),
+  csso        = require('gulp-csso'),*/
   jade        = require('gulp-jade'),
   browserify  = require('gulp-browserify'),
   livereload  = require('gulp-livereload'), // Livereload plugin needed: https://chrome.google.com/webstore/detail/livereload/jnihajbhpnppcggbcgedagnkighmdlei
@@ -9,51 +9,80 @@ var gulp      = require('gulp'),
   express     = require('express'),
   app         = express(),
   path        = require('path'),
-  server      = tinylr();
+  server      = tinylr(),
+  config      = require('./config')()[process.env.NODE_ENV],
+  replace     = require('gulp-replace'),
+  rimraf      = require('gulp-rimraf'),
+  sftp        = require('gulp-sftp');
 
+/*
+* Cleans the dist folder
+*/
+gulp.task('clean', function () {
+  return gulp.src('dist/**/*', { read: false }).
+    pipe(rimraf({ force: true }));
+});
 
-// --- Basic Tasks ---
-/*gulp.task('css', function() {
-  return gulp.src('src/assets/stylesheets/*.styl').
-    pipe( styl( { whitespace: true } ) ).
-    pipe( csso() ).
-    pipe( gulp.dest('dist/assets/stylesheets/') ).
-    pipe( livereload( server ));
-});*/
+/*
+* Copies the contents on dist folder to the specified server
+*/
+gulp.task('sftp-copy', function () {
+  return gulp.src('dist/**/*').
+    pipe(sftp({
+          host: config.deploy.host,
+          user: config.deploy.user,
+          pass: config.deploy.pass
+          port: config.deploy.port
+          remotePath: config.deploy.remotePath
+        }));
+});
 
-gulp.task('browserify', function() {
+/*
+* Browserify's app.js file
+*/
+gulp.task('browserify', function () {
   return gulp.src('src/app.js').
-    pipe( browserify( {debug : true}) ).
-    pipe( gulp.dest('dist/')).
-    pipe( livereload( server ));
-});
-
-gulp.task('templates', function() {
-  return gulp.src('src/index.jade').
-    pipe(jade({
-      pretty: true
-    })).
+    pipe(replace(/\{serviceUrl\}/g, config.serviceUrl)).
+    pipe(browserify({ debug : true })).
     pipe(gulp.dest('dist/')).
-    pipe( livereload( server ));
+    pipe(livereload(server));
 });
 
-gulp.task('angular-views', function() {
+/*
+* Parses the index.jade and generates dist/index.html
+*/
+gulp.task('templates', function () {
+  return gulp.src('src/index.jade').
+    pipe(jade({ pretty: true })).
+    pipe(gulp.dest('dist/')).
+    pipe(livereload(server));
+});
+
+/*
+* Copies src/views to dist/views
+*/
+gulp.task('angular-views', function () {
   return gulp.src('src/views/**/*.html').
-    pipe(gulp.dest('dist/views/')).
-    pipe( livereload( server ));
+    pipe(gulp.dest('dist/views/') ).
+    pipe(livereload(server));
 });
 
-gulp.task('express', function() {
+/*
+* Dev server
+*/
+gulp.task('express', function () {
   app.use(express.static(path.resolve('./dist')));
   app.listen(1337);
   gutil.log('Listening on port: 1337');
 });
 
+/*
+* Re-executes tasks if any file changes
+*/
 gulp.task('watch', function () {
   server.listen(35729, function (err) {
     if (err) return console.log(err);
 
-    //gulp.watch('src/assets/stylesheets/*.styl',['css']);
     gulp.watch('src/**/*.js',['browserify']);
     gulp.watch('src/*.jade',['templates']);
     gulp.watch('src/views/**/*.html',['angular-views']);
@@ -61,5 +90,6 @@ gulp.task('watch', function () {
   });
 });
 
-// Default Task
+gulp.task('deploy', ['build', 'sftp-copy']);
+gulp.task('build', ['clean', 'browserify', 'templates', 'angular-views']);
 gulp.task('default', ['browserify', 'templates', 'angular-views', 'express', 'watch']);
