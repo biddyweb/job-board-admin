5b38d8f6467aad4a4028fbdb4ae6afe426935704
Gabriel Chertok <gchertok@gmail.com>
2014-09-08 19:09:28 -0300

    updated Gulp to use sequenced build

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,6 +2,7 @@ var gulp      = require('gulp'),
   gutil       = require('gulp-util'),
   jade        = require('gulp-jade'),
   browserify  = require('gulp-browserify'),
+  stylus      = require('gulp-stylus'),
   livereload  = require('gulp-livereload'), // Livereload plugin needed: https://chrome.google.com/webstore/detail/livereload/jnihajbhpnppcggbcgedagnkighmdlei
   tinylr      = require('tiny-lr'),
   express     = require('express'),
@@ -11,20 +12,21 @@ var gulp      = require('gulp'),
   config      = require('./config')()[process.env.NODE_ENV],
   replace     = require('gulp-replace'),
   rimraf      = require('gulp-rimraf'),
-  sftp        = require('gulp-sftp');
+  sftp        = require('gulp-sftp'),
+  runSequence = require('run-sequence');
 
 /*
 * Cleans the dist folder
 */
 gulp.task('clean', function () {
-  return gulp.src('dist/**/*', { read: false }).
+  return gulp.src('dist/', { read: false }).
     pipe(rimraf({ force: true }));
 });
 
 /*
 * Copies the contents on dist folder to the specified server
 */
-gulp.task('deploy', ['clean', 'browserify', 'templates', 'angular-views'], function () {
+gulp.task('sftp', function () {
   return gulp.src('dist/**/*').
     pipe(sftp({
           host: config.deploy.host,
@@ -36,12 +38,22 @@ gulp.task('deploy', ['clean', 'browserify', 'templates', 'angular-views'], funct
 });
 
 /*
+* Parses styl templates
+*/
+gulp.task('stylus', function () {
+  gulp.src('src/*.styl').
+    pipe(stylus()).
+    pipe(gulp.dest('dist/')).
+    pipe(livereload(server));
+});
+
+/*
 * Browserify's app.js file
 */
-gulp.task('browserify', ['clean'], function () {
+gulp.task('browserify', function () {
   return gulp.src('src/app.js').
     pipe(replace(/\{serviceUrl\}/g, config.serviceUrl)).
-    pipe(browserify({ debug : true })).
+    pipe(browserify({ debug : process.env.NODE_ENV == 'development' })).
     pipe(gulp.dest('dist/')).
     pipe(livereload(server));
 });
@@ -49,7 +61,7 @@ gulp.task('browserify', ['clean'], function () {
 /*
 * Parses the index.jade and generates dist/index.html
 */
-gulp.task('templates', ['clean'], function () {
+gulp.task('templates', function () {
   return gulp.src('src/index.jade').
     pipe(jade({ pretty: true })).
     pipe(gulp.dest('dist/')).
@@ -59,9 +71,18 @@ gulp.task('templates', ['clean'], function () {
 /*
 * Copies src/views to dist/views
 */
-gulp.task('angular-views', ['clean'], function () {
+gulp.task('angular-views', function () {
   return gulp.src('src/views/**/*.html').
     pipe(gulp.dest('dist/views/') ).
+    pipe(livereload(server));
+});
+
+/*
+* Copies src/*.txt to dist. This is useful for robots.txt and humans.txt
+*/
+gulp.task('text-files', function () {
+  return gulp.src('src/*.txt').
+    pipe(gulp.dest('dist/') ).
     pipe(livereload(server));
 });
 
@@ -83,10 +104,18 @@ gulp.task('watch', function () {
 
     gulp.watch('src/**/*.js',['browserify']);
     gulp.watch('src/*.jade',['templates']);
+    gulp.watch('src/*.styl',['stylus']);
     gulp.watch('src/views/**/*.html',['angular-views']);
 
   });
 });
 
-gulp.task('build', ['clean', 'browserify', 'templates', 'angular-views']);
-gulp.task('default', ['browserify', 'templates', 'angular-views', 'express', 'watch']);
+gulp.task('build', function () {
+  runSequence('clean', 'browserify', ['angular-views', 'templates', 'stylus', 'text-files']);
+});
+
+gulp.task('deploy', function () {
+  runSequence('clean', 'browserify', ['angular-views', 'templates', 'stylus', 'text-files'], 'sftp');
+})
+
+gulp.task('default', ['browserify', 'templates', 'angular-views', 'text-files', 'stylus', 'express', 'watch']);
